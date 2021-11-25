21ef15a74d31197d31012a8ea0f3bafbecb5e5f5
Shahmir Hussain <shahmir.hussain@internative.net>
2021-11-26 02:40:46 +0500

    many changes made

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 <script>
   AOS.init();
 </script>
+
+
+let womanButton = document.getElementById('nav-profile-tab');
+womanButton.onclick = () => {
+  document.getElementById('man-image').innerHTML('girl-image');
+};
